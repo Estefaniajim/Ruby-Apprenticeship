0a42e8a4e7456dad207d9f9dbc1d4b1eccf31e3b
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-01-06 15:51:52 -0600

    Update Challenge1.rb

--- Challenge1.rb
+++ Challenge1.rb
@@ -1,2 +1,22 @@
-letters = ("a".."x").to_a.map {|x| x}
-p letters
+input = "aabbbbb".chars
+alphabet = ("a".."z").to_a.map {|x| x}
+dict = Hash.new
+
+alphabet.each do |char|
+  dict[char] = 0
+end
+
+input.each do |char|
+  dict[char] += 1
+end
+
+preVal = dict["a"]
+alphabet = ("b".."z").to_a.map {|x| x}
+
+alphabet.each do |char|
+  if dict[char] > preVal
+    return false
+  end
+  preVal = dict[char]
+end
+return true
