a05505c5be4460f0788f6a9f9d0007eb76c74cf6
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-01-06 14:31:08 -0600

    Update Conditionals.rb

---
+++ Conditionals.rb
@@ -0,0 +1,37 @@
+x = 10
+y = 100
+z = 10
+
+#if statement
+if x == y
+  puts true
+else
+  puts false
+end
+
+#unless statement
+players = ["Correa", "Carter", "Altuve"]
+unless players.empty?
+  players.each {|player| puts player}
+end
+#one line
+players.each {|player| puts player} unless players.empty?
+
+#multiple if else conditionals (elsif)
+if x == y
+  puts "x is equal to y"
+elsif x >= z
+  puts "x is greater than z"
+else
+  puts "something else"
+end
+
+#compound conditionals
+#or
+if x == y || x == z
+  puts "from the or"
+end
+# and
+if (x == 10 && x == z) || (x == y)
+  puts "from and"
+end
