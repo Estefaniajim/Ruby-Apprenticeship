4a6bdce5086dc06944d4cfb1ea898d64c80c3509
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-01-06 14:10:11 -0600

    Update Hashes.rb

--- Hashes.rb
+++ Hashes.rb
@@ -12,7 +12,32 @@ p positions
 positions.each_key do |key|
   p key
 end
+
 #iterate over the values
 positions.each_value do |value|
   p value
 end
+
+#add a key
+positions[:forth] = "coco"
+p positions
+
+#invert the values for the keys
+inverted = positions.invert
+p inverted
+
+#merge two hashes
+merged = inverted.merge(positions)
+p merged
+
+#make it a matrix
+matrix = positions.to_a
+p matrix
+
+#keys as an array
+keys = positions.keys
+p keys
+
+#values as an array
+values = positions.values
+p values
